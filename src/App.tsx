a748f7080f836e9bdc8a5631e86b65cfc4c502f8
Karol <114170012+karol199393@users.noreply.github.com>
2023-11-20 08:09:53 +0100

    Dodanie części projektu React

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// @ts-ignore
+import React, { useEffect, useState } from 'react';
+import './styles.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+interface Druzyna {
+    id: number;
+    nazwa: string;
 }
+
+export const ListaDruzyn: React.FC<{ druzyny: Druzyna[] }> = ({ druzyny }) => (
+    <div className="container">
+        <h2>Lista Drużyn:</h2>
+        <ul>
+            {druzyny.map((druzyna) => (
+                <li key={druzyna.id} className="team-item">{druzyna.nazwa}</li>
+            ))}
+        </ul>
+    </div>
+);
+
+const App: React.FC = () => {
+    const [druzyny, setDruzyny] = useState<Druzyna[]>([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch('/api/teams/getTeams'); // Poprawiony endpoint
+                const data = await response.json();
+                setDruzyny(data);
+            } catch (error) {
+                console.error('Błąd pobierania drużyn:', error);
+                console.error('Błąd pobierania drużyn:', error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    return (
+        <div className='container'>
+            <h1>Aplikacja do Zarządzania Drużynami</h1>
+            <ListaDruzyn druzyny={druzyny} />
+        </div>
+    );
+};
 
 export default App;
